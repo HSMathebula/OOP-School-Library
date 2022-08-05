167e4a0aae82531dfb2f5c5f9b6a9a3d5e0035a6
HSMathebula <56921581+HSMathebula@users.noreply.github.com>
2022-08-05 11:58:07 +0200

    Add menu and case statements

--- app.rb
+++ app.rb
@@ -9,4 +9,36 @@ class App
     @people = []
     @rentals = []
   end
+
+  def menu
+    puts ''
+    puts 'Please choose an option by entering a number:'
+    puts '1 - List all books'
+    puts '2 - List all people'
+    puts '3 - Create a person'
+    puts '4 - Create a book'
+    puts '5 - Create a rental'
+    puts '6 -  List all rentals for a given person id'
+    puts '7 - Exit'
+  end
+
+  def option_checker(answer)
+    case answer
+    when 1
+      list_books
+    when 2
+      list_people
+    when 3
+      create_person
+    when 4
+      create_book
+    when 5
+      create_rental
+    when 6
+      list_rentals
+    else
+      puts 'Wrong input!'
+      run
+    end
+  end
 end
