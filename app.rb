342735327cd7af40ad8d3612736f1d0fcfcc7dca
HSMathebula <56921581+HSMathebula@users.noreply.github.com>
2022-08-05 12:12:12 +0200

    Create rental methode

--- app.rb
+++ app.rb
@@ -116,6 +116,27 @@ class App
     run
   end
 
+  def create_rental
+    puts 'Select the book from the following list by number (not ID)'
+    @books.each_with_index do |book, idx|
+      puts "#{idx}) Title: '#{book.title}', Author: '#{book.author}'"
+    end
+    selected_id_book = gets.chomp.to_i
+    print 'Date: '
+    date = gets.chomp
+
+    puts 'Select a person from the following list by number (not ID)'
+    @people.each_with_index do |person, idx|
+      puts "#{idx}) [#{person.class.name}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+    end
+    selected_id_person = gets.chomp.to_i
+
+    rental = Rental.new(date, @books[selected_id_book], @people[selected_id_person])
+    @rentals.push(rental)
+    puts 'Rental created successfully'
+    run
+  end
+
   def list_books
     puts 'List of all the books in the library:'
     @books.each do |book|
