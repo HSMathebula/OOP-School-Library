a80a9a2ca3d46b7f050989cfb0c30f8617ff48f3
HSMathebula <56921581+HSMathebula@users.noreply.github.com>
2022-08-12 15:12:22 +0200

    Fix linter errors

--- person.rb
+++ person.rb
@@ -5,6 +5,7 @@ class Person < Nameable
   attr_accessor :age, :name, :rentals
 
   attr_reader :id
+
   def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
--- rental.rb
+++ rental.rb
@@ -4,7 +4,7 @@ require_relative './person'
 class Rental
   attr_accessor :date
   attr_reader :book, :person
-  
+
   def initialize(date, book, person)
     @date = date
     @person = person
