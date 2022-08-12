9aa951e8c6665ab873fd57a7e2148163a59cb9c9
Felix Odete <felix.odette@gmail.com>
2022-08-12 17:39:23 +0300

    Linter fix

--- spec/teacher_spec.rb
+++ spec/teacher_spec.rb
@@ -3,7 +3,7 @@ require './teacher'
 describe Teacher do
   context 'Instantiate teacher' do
     teacher = Teacher.new(45, 'Maths', 'TEST')
-    
+
     it 'Age is correct' do
       expect(teacher.age).to eq 45
     end
